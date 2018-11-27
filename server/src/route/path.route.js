1508d158f34a2d013ae0b0af727d0a32167e5500
Paul Jackob <pauljackob911@hotmail.com>
2018-11-27 12:21:16 +0100

    fix route

--- server/src/route/path.route.js
+++ server/src/route/path.route.js
@@ -2,10 +2,10 @@ const paths = require('../controller/path.controller.js');
 const modules = require('../controller/module.controller.js');
 
 path.exports = (app) => {
-  app.get('/path', paths.findAll);
-  app.get('/path/:pathId', paths.findOne);
-  app.post('/path', paths.create);
-  app.post('/path/:pathId/module', modules.create);
-  app.delete('/path/:pathId', paths.destroy);
-  app.patch('/path/:pathId', paths.update);
+  app.get('/api/path', paths.findAll);
+  app.get('/api/path/:pathId', paths.findOne);
+  app.post('/api/path', paths.create);
+  app.post('api//path/:pathId/module', modules.create);
+  app.delete('/api/path/:pathId', paths.destroy);
+  app.patch('/api/path/:pathId', paths.update);
 };
